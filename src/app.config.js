6531db831deb817d25f67cbf968ac2e373546157
vanishreeva <vanishreeva6659@gmail.com>
2023-03-07 06:03:18 +0000

    Changed group name

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Group 3';
+    team = 'Leviosa';
     version = '1.0.0';
 }
 module.exports = AppConfig;
